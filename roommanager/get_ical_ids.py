f1750d4c6beeaac826ae843c579a5dae886a4af5
Jan-Niklas FICHTE <s170657@student.dhbw-mannheim.de>
2019-05-09 16:39:04 +0200

    Update Function and Views included

--- roommanager/get_ical_ids.py
+++ roommanager/get_ical_ids.py
@@ -1,34 +1,8 @@
 import requests
 import re
 import os
-from datetime import datetime, timedelta, timezone
 import icalendar
-from dateutil.rrule import *
-"""
-allUIDs = []
-coursesSubsite = requests.get("https://vorlesungsplan.dhbw-mannheim.de")
-coursesSubsite = str(coursesSubsite.content)
-coursesSubsiteIds = re.findall("gid=[0-9]*", coursesSubsite)
-#print(coursesSubsiteIds)
-for ids in coursesSubsiteIds:
-    url = "https://vorlesungsplan.dhbw-mannheim.de/index.php?action=list&" + ids
-    #print(url)
-    curRequest = requests.get(url)
-    courseSite = str(curRequest.content)
-    #print(courseSite)
-    courseSiteUids = re.findall("uid=[0-9]*", courseSite)
-    courseSiteUids = str(courseSiteUids)
-    allUIDs.append(str(courseSiteUids))
 
-allUIDs = str(allUIDs)
-allUIDs = allUIDs.replace("[", '')
-allUIDs = allUIDs.replace("]", '')
-allUIDs = allUIDs.replace("\"", '')
-print(str(allUIDs))
-
-#print(allGIDs)
-#print(x)
-"""
 
 download_path = os.path.dirname(os.path.realpath(__file__)) + "\icals\\"
 def download_icals():
@@ -47,6 +21,7 @@ def download_icals():
             ical = requests.get(url)
             with open(download_url, 'wb') as f:
                 f.write(ical.content)
+    return index
 
 
 def download_update_icals():
@@ -67,7 +42,8 @@ def download_update_icals():
             with open(download_url, 'wb') as f:
                 f.write(ical.content)
     return index
-d = []
+
+
 def analyse_icals(range1, range2, filenameflag):
     num_files = len([f for f in os.listdir(download_path)if os.path.isfile(os.path.join(download_path, f))])
     for x in range(range1, range2):
@@ -103,13 +79,13 @@ def analyse_icals(range1, range2, filenameflag):
                     event_json[location].append(date_set)
 
             read_ical.close()
-        print(event_json)
+        #print(event_json)
         return event_json
         #print(date_set)
 
 
 def update_icals():
-    length = download_update_icals()
+    length = 284
     for x in range(1, length):
         old_ical = download_path + str(x) + ".ical"
         new_ical = download_path + "i" + str(x) + ".ical"
@@ -118,16 +94,14 @@ def update_icals():
         for line_old in old_content:
             for line_new in new_content:
                 if line_new != line_old:
-                    old_dict = analyse_icals(x, x, old_content)
-                    new_dict = analyse_icals(x, x, new_content)
+                    old_dict = analyse_icals(x, x + 1, "old")
+                    #print(old_dict)
+                    new_dict = analyse_icals(x, x +1, "new")
+                    #print(new_dict)
                     compare_dict(old_dict, new_dict)
-
-
-                    #os.remove(old_ical)
-                    #os.rename(new_ical, download_path + str(x) + ".ical")
-
-
-        #print(content)
+                    break
+                else:
+                    print("okay")
 
 
 def compare_dict(old_dict, new_dict):
@@ -137,22 +111,6 @@ def compare_dict(old_dict, new_dict):
     new_val = oldd_keys - newd_keys
     rem_val = newd_keys - oldd_keys
     mod_val = { i : (old_dict[i], new_dict[i]) for i in intersect_keys if old_dict[i] != new_dict[i]}
-    print(new_val)
-    print(rem_val)
-    print(mod_val)
-download_icals()
+    #print(mod_val)
+update_icals()
 
-
-"""
-UIDs = str(UIDs)
-UIDs = UIDs.replace('value=', '')
-UIDs = UIDs.replace('\"', '')
-print(UIDs[2:(UIDs.find(',')-2)])
-print(UIDs)
-for ids in UIDs:
-    url = "http://vorlesungsplan.dhbw-mannheim.de/ical.php?uid=" + ids
-    print(url)
-    ical = requests.get(url)
-    ical = str(ical.content)
-
-"""
--- roommanager/views.py
+++ roommanager/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from roommanager import  get_ical_ids
+
+def download_and_analyse():
+    i = get_ical_ids.download_update_icals()
+    get_ical_ids.analyse_icals(1, i, "new")
 
 
 @login_required(login_url='login/')
