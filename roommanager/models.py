a5b4e8a7412854dc2e6bd955bf973ba20bc17a17
Jan-Niklas FICHTE <s170657@student.dhbw-mannheim.de>
2019-05-16 17:57:59 +0200

    Added WeatherForecast Model and Logic to compare

--- roommanager/models.py
+++ roommanager/models.py
@@ -8,3 +8,8 @@ class Rooms(models.Model):
     room = models.CharField(max_length=100)
     date = models.DateField()
     slotid = models.ForeignKey(Slots, on_delete=models.CASCADE)
+
+class Forecast(models.Model):
+    forecastdate = models.DateField()
+    forecasttime = models.TimeField()
+    temp = models.FloatField()
--- roommanager/openweather.py
+++ roommanager/openweather.py
@@ -1,26 +1,34 @@
 import pyowm
-from time import gmtime, strftime
-
+import datetime
+from time import gmtime, strftime, ctime
+from roommanager.models import Forecast
 def getCurrentWeather():
     owm = pyowm.OWM('d12bf70afc7467861163ebdc67a1642d')
     fore = owm.three_hours_forecast('Mannheim')
     forecast = fore.get_forecast()
-    print(forecast.get_reception_time('date'))
-    print(len(forecast))
-    list = []
+    forecast_list = []
     weather_list = forecast.get_weathers()
     for weather in weather_list:
-        date = weather.get_reference_time('iso')
+        temp_date_ob = datetime.datetime.strptime(weather.get_reference_time('iso'), '%Y-%m-%d %H:%M:%S+%f')
+        weather_date = temp_date_ob.date()
+        weather_time = temp_date_ob.time()
         temp = str(weather.get_temperature()["temp"])
-        list.append([date, temp])
-    return list
+        forecast_list.append([ weather_date, weather_time, temp])
+        forecast = Forecast(forecastdate=weather_date, forecasttime=weather_time, temp=temp)
+        forecast.save()
+    return forecast_list
 
 
 def analyseWeather(forecast_list):
-    current_time = strftime('%Y-%m-%d %H:%M:%S', gmtime())
-    print(current_time)
-    for date, weather in forecast_list:
-        print(date + weather)
+    current_dates = datetime.datetime.strptime(strftime('%Y-%m-%d %H:%M:%S', gmtime()), '%Y-%m-%d %H:%M:%S')
+    current_time = current_dates.time()
+    current_date = current_dates.date()
+    for forecast_date, forecast_time, weather in forecast_list:
+        if forecast_date > current_date:
+            print(forecast_date)
+
+
+
 
 currentWeatherList = getCurrentWeather()
 analyseWeather(currentWeatherList)
