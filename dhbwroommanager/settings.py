f9786fc0d934ea4ba5c7416e7c133845eb881ea3
Justus-Jonas ERKER <Justus-Jonas.ERKER@de.amadeus.com>
2019-05-16 16:24:53 +0200

    images available

--- dhbwroommanager/settings.py
+++ dhbwroommanager/settings.py
@@ -123,6 +123,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    #"django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 LOGIN_REDIRECT_URL = 'main'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
