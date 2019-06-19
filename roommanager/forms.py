074af46806dcb9111ff674a1e64cd44a6c1aad5b
Dominique Lasserre <lasserre.d@gmail.com>
2019-06-20 00:05:18 +0200

    Simplify form

--- roommanager/forms.py
+++ roommanager/forms.py
@@ -5,17 +5,13 @@ from roommanager.models import Rooms
 
 
 class RoomForm(forms.Form):
-    class Meta:
-        model = Rooms
-
-    g_name = forms.CharField(label='Group name', max_length=50)
+    groupName = forms.CharField(label='Group name', max_length=50)
     #  start_time = forms.TimeField(widget=dhbwroommanager.SelectTimeWidget())
     #self.start_time = forms.TimeField(label="from")
     # #self.stop_time = forms.TimeField(label='to')
-    hourS = forms.ChoiceField(choices=[(x, x) for x in range(8, 21)])
-    hourS.label = "Start time"
-    minS = forms.ChoiceField(choices=[(x, x) for x in range(0, 60, 15)])
-    hourE = forms.ChoiceField(choices=[(x, x) for x in range(0, 3)])
-    hourE.label = "Duration"
-    minE = forms.ChoiceField(choices=[(x, x) for x in range(0, 60, 15)])
-
+    #hourS = forms.ChoiceField(choices=[(x, x) for x in range(8, 21)])
+    #hourS.label = "Start time"
+    #minS = forms.ChoiceField(choices=[(x, x) for x in range(0, 60, 15)])
+    #minS.label = "Mins"
+    duration = forms.ChoiceField(choices=[(x, x) for x in range(15, 135, 15) if x < 60 or x % 30 == 0])
+    duration.label = "Duration"
