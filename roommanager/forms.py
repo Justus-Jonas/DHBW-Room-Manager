d6dcb028c864d521f6f51db2be53c66a3af7c29c
Justus-Jonas ERKER <Justus-Jonas.ERKER@de.amadeus.com>
2019-06-19 14:29:51 +0200

    fix

--- roommanager/forms.py
+++ roommanager/forms.py
@@ -1,18 +1,21 @@
 from django import forms
 from django.forms import widgets
 
+from roommanager.models import Rooms
+
 
 class RoomForm(forms.Form):
     class Meta:
         model = Rooms
 
-    def __init__(self, *args, **kwargs):
-        super(RoomForm, self).__init__(*args, **kwargs)
-        self.fields['mydate'].widget = widgets.AdminDateWidget()
-        self.fields['mytime'].widget = widgets.AdminTimeWidget()
-        self.fields['mydatetime'].widget = widgets.AdminSplitDateTime()
-        g_name = forms.CharField(label='Group name', max_length=50)
-        #  start_time = forms.TimeField(widget=dhbwroommanager.SelectTimeWidget())
-        self.start_time = forms.TimeField(label="from")
-        self.stop_time = forms.TimeField(label='to')
-        # will be filled in by django
+    g_name = forms.CharField(label='Group name', max_length=50)
+    #  start_time = forms.TimeField(widget=dhbwroommanager.SelectTimeWidget())
+    #self.start_time = forms.TimeField(label="from")
+    # #self.stop_time = forms.TimeField(label='to')
+    hourS = forms.ChoiceField(choices=[(x, x) for x in range(8, 21)])
+    hourS.label = "Start time"
+    minS = forms.ChoiceField(choices=[(x, x) for x in range(0, 60, 15)])
+    hourE = forms.ChoiceField(choices=[(x, x) for x in range(0, 3)])
+    hourE.label = "Duration"
+    minE = forms.ChoiceField(choices=[(x, x) for x in range(0, 60, 15)])
+
