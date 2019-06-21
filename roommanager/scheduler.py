ef96797e5e3cb5b54638614873fc29a84ae100a1
JaNiFi <s170657@student.dhbw-mannheim.de>
2019-06-21 17:56:35 +0200

    Scheduled Analyse

--- roommanager/scheduler.py
+++ roommanager/scheduler.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
 from roommanager.openweather import getCurrentWeather
-
+from roommanager.get_ical_ids import download_and_analyse
 
 def start():
     sched = BlockingScheduler()
     scheduler = BackgroundScheduler()
     scheduler.add_job(getCurrentWeather, 'interval', hours=3)
     scheduler.start()
-    #sched.add(#updateFunktionHierReinRestPräpraiert, 'cron', hour=2)
+    sched.add_job(download_and_analyse, 'cron', hour=2)
