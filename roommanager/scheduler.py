57b4b3eae3640d6cdbb7ec07b903f584fd23684d
JaNiFi <s170657@student.dhbw-mannheim.de>
2019-06-22 05:56:35 +0200

    changed hour

--- roommanager/scheduler.py
+++ roommanager/scheduler.py
@@ -5,8 +5,8 @@ from roommanager.get_ical_ids import download_and_analyse
 """Scheduler for Weather API Calls and Icals Update"""
 def start():
     scheduler = BackgroundScheduler()
-    """Getting the latest temperature every 3 hours"""
-    scheduler.add_job(getCurrentWeather, 'interval', hours=3)
+    """Getting the latest temperature every hour"""
+    scheduler.add_job(getCurrentWeather, 'interval', hours=1)
     """Updating the icals every day at 2am"""
     scheduler.add_job(download_and_analyse, 'cron', hour=2)
     scheduler.start()
