17e382589131087a929d97701ee46d5e8b261251
JaNiFi <s170657@student.dhbw-mannheim.de>
2019-06-22 05:31:02 +0200

    Fixed Job and tested

--- roommanager/scheduler.py
+++ roommanager/scheduler.py
@@ -4,11 +4,10 @@ from roommanager.openweather import getCurrentWeather
 from roommanager.get_ical_ids import download_and_analyse
 """Scheduler for Weather API Calls and Icals Update"""
 def start():
-    sched = BlockingScheduler()
     scheduler = BackgroundScheduler()
     """Getting the latest temperature every 3 hours"""
     scheduler.add_job(getCurrentWeather, 'interval', hours=3)
     """Updating the icals every day at 2am"""
-    sched.add_job(download_and_analyse, 'cron', hour=2)
+    scheduler.add_job(download_and_analyse, 'cron', hour=2)
     scheduler.start()
 
